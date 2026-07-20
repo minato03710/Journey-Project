d9b6768022a55afdd986417c1754a4f8e632ccd5
Souppy0 <bobbinsoup16@gmail.com>
2026-07-21 06:41:15 +1200

    butterfly + respawn 2s after death to main menu
    butterfly + respawn 2s after death to main menu

--- Assets/Gallium/Scripts7/GameManager.cs
+++ Assets/Gallium/Scripts7/GameManager.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 using TMPro;
-
+using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
@@ -17,10 +17,17 @@ public class GameManager : MonoBehaviour
         gameOverText.SetActive(false);
     }
 
-    public void GameOver()
+public void GameOver()
     {
         gameOverText.SetActive(true);
 
-        Time.timeScale = 0;
+        // Schedules LoadMainMenu to run after 2 seconds
+        Invoke(nameof(LoadMainMenu), 2.0f);
+    }
+
+    private void LoadMainMenu()
+    {
+        Time.timeScale = 1f; 
+        SceneManager.LoadScene("MainMenu");
     }
 }
--- Assets/Isaac/MainMenuManage.cs
+++ Assets/Isaac/MainMenuManage.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 public class MainMenuManager : MonoBehaviour
 {
     // This variable holds the text "UI"
-    [SerializeField] private string gameplaySceneName = "UI"; 
+    [SerializeField] private string gameplaySceneName = "GameScene"; 
 
     public void OnPlayButtonClicked()
     {
